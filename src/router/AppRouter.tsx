f318a3dbe29c77d24d1a320931967d14dcf2f203
axmadbek0 <axmadbek0990@gmail.com>
2026-07-01 10:47:58 +0500

    birinchi ish

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import {HomePage} from "../pages/HomePage";
 import {ProductPage} from "../pages/ProductPage";
 import {CartPage} from "../pages/CartPage";
 import {CatalogPage} from "../pages/CatalogPage";
+import {OrdersPage} from "../pages/OrdersPage";
 
 export function AppRouter() {
     return (
@@ -13,8 +14,8 @@ export function AppRouter() {
                 <Route path="/product/:id" element={<ProductPage />} />
                 <Route path="/cart" element={<CartPage />} />
                 <Route path="/catalog" element={<CatalogPage />} />
+                <Route path="/orders" element={<OrdersPage />} />
             </Routes>
         </BrouserRouter>
     );
 }
-
